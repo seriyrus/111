8424cc07100643ca060361c84464e3f13bc64034
seriyrus <postolenko00@bk.ru>
2025-06-10 09:53:36 +0300

    11111

--- Counter/main/models.py
+++ Counter/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.course
+
+class Student(models.Model):
+    surname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    course = models.ManyToOneRel(on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return f'{self.surname} {self.first_name} {self.last_name}: {self.course}'
